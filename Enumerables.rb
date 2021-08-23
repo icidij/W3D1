0265e182f3e4c750e14da453ac54f1c23152d385
Matthew Swedin <matthewswedin@matthews-mbp-2.home>
2021-08-23 15:13:06 -0400

    my_any?

--- Enumerables.rb
+++ Enumerables.rb
@@ -14,15 +14,21 @@ class Array
         output = []
         self.my_each { |ele| output << ele if prc.call(ele) }
         output
-
     end
+
     def my_reject(&prc)
         output = []
         self.my_each {|ele| output << ele if !prc.call(ele)}
         output
-
-
     end
+
+    def my_any?(&prc)
+        self.my_each {|ele| return true if prc.call(ele)}
+        false
+    end
+
+
+
 
 
 
