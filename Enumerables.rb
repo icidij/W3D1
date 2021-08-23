ef1d90f3ba3a5513e8e7cca4a4f1f1bc56358973
Matthew Swedin <matthewswedin@matthews-mbp-2.home>
2021-08-23 14:54:53 -0400

    my_select

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,13 +1,20 @@
 class Array
 
-def my_each(&prc)
+    def my_each(&prc)
 
-    i = 0
-    while i <= self.length - 1
-        prc.call(self[i])
-        i += 1
+        i = 0
+        while i <= self.length - 1
+            prc.call(self[i])
+            i += 1
+        end
+        self
     end
-    self
+
+    def my_select(&prc)
+        output = []
+        self.my_each { |ele| output << ele if prc.call(ele) }
+        output
+    end
 end
 
 
@@ -22,4 +29,3 @@ end
 
 
 
-end
